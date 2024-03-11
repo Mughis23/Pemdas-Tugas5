bf93eaaf67624755006594b39693734a32202fff
Mughis Fadhil <mughisfar@gmail.com>
2024-03-11 12:07:14 +0700

    Update Repo

--- Tugas Sesi 2/ganjilGenap.py
+++
@@ -1,8 +0,0 @@
-angka = int(input("Masukan angka yang anda inginkan : "))
-
-if angka % 2 == 0:
-    result = "merupakan bilangan genap"
-else : 
-    result = "merupakan bilangan ganjil"
-
-print(angka, result)
