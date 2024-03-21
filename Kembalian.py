3458aa976019d7de3dc41c2d1a5f1bf09f4689ff
Mughisfar <91643438+Mughis23@users.noreply.github.com>
2024-03-21 23:54:12 +0700

    Delete Kembalian.py

--- Kembalian.py
+++
@@ -1,4 +0,0 @@
-uangAwal = int(input("Masukan uang yang anda miliki : "))
-uangKeluar = int(input("Masukan uang yang anda keluarkan : "))
-kembalian = uangAwal - uangKeluar
-print ("Uang kembalian anda Rp.%i"%kembalian)
